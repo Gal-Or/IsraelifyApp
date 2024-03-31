447af236a0e97aa6703bea43c20627fe3cf6d374
barmoshe <1barmoshe1@gmail.com>
2024-03-31 15:13:03 +0300

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,43 @@
-import { HomePage } from './pages/HomePage.jsx'
+import { HomePage } from "./pages/HomePage.jsx";
+import { SearchPage } from "./pages/SearchPage.jsx";
+import { StationPage } from "./pages/StationPage.jsx";
+import { SignInPage } from "./pages/SignInPage.jsx";
+import { SignUpPage } from "./pages/SignUpPage.jsx";
+
+//home-page,signup-page,signin-page,search-page,station-page
 
 // Routes accesible from the main navigation (in AppHeader)
 const routes = [
-    {
-        path: '/',
-        component: <HomePage />,
-        label: 'Home 🏠',
-    }
-]
+  {
+    path: "/",
+    component: <HomePage />,
+    label: "Home",
+  },
+  {
+    path: "/search",
+    component: <SearchPage />,
+    label: "Search",
+  },
+  {
+    path: "/search/:query",
+    component: <SearchPage />,
+    label: "Search",
+  },
+  {
+    path: "/station/:stationId",
+    component: <StationPage />,
+    label: "Station",
+  },
+  {
+    path: "/signin",
+    component: <SignInPage />,
+    label: "Sign In",
+  },
+  {
+    path: "/signup",
+    component: <SignUpPage />,
+    label: "Sign Up",
+  },
+];
 
-export default routes
+export default routes;
