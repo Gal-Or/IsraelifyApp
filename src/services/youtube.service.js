b36336f2bf0b9d91a0f69394abf9e734a539ea0d
barmoshe <1barmoshe1@gmail.com>
2024-04-01 19:14:37 +0300

    Update youtube.service.js

--- src/services/youtube.service.js
+++ src/services/youtube.service.js
@@ -2,10 +2,10 @@ export const youtubeService = {
   query,
 };
 
-async function query(searchStr) {
+async function query(searchStr, maxResults = 50) {
   const apiKey = "AIzaSyDexrW1icUvoT85OYB-vDlsxAs4UQkpcfw"; // Replace with your actual API key
   const response = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchStr}&key=${apiKey}&maxResults=50&type=video`
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchStr}&key=${apiKey}&maxResults=${maxResults}&type=video`
   );
   const data = await response.json();
   return data.items;
