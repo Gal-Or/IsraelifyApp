c5fe8e75231ef28dbd712e2e60ddbecf66034740
galor <galor.dvlp@gmail.com>
2024-04-01 12:46:03 +0300

    Create station service

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,8 @@ import { AppHeader } from "../cmps/AppHeader.jsx";
 import { Recommendations } from "../cmps/Recommendations.jsx";
 import { Catalog } from "../cmps/Catalog.jsx";
 
+import { stationService } from "../services/station.service.js"
+
 export function HomePage() {
   return (
     <section>
