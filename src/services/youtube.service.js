b911ea30fc9cf866acfee5401b703f3ef0ac8cc5
barmoshe <1barmoshe1@gmail.com>
2024-04-02 12:15:04 +0300

    update youtube api key

--- src/services/youtube.service.js
+++ src/services/youtube.service.js
@@ -2,10 +2,10 @@ export const youtubeService = {
   query,
 };
 
-async function query(searchStr, maxResults = 50) {
-  const apiKey = "AIzaSyDexrW1icUvoT85OYB-vDlsxAs4UQkpcfw"; // Replace with your actual API key
+async function query(searchStr, maxResults = 10) {
+  const apiKey = "AIzaSyDyENi9CUDP6AY90P7xZm3x326KlwI_d2Q"; // Replace with your actual API key
   const response = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchStr}&key=${apiKey}&maxResults=${maxResults}&type=video`
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchStr}&key=${apiKey}&maxResults=${maxResults}`
   );
   const data = await response.json();
   return data.items;
