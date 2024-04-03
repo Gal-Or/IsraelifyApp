3250aeb2ac0a8351312c1dd2adab41d0223967f0
barmoshe <1barmoshe1@gmail.com>
2024-04-03 13:59:34 +0300

    fix youtube search - get only video type

--- src/services/youtube.service.js
+++ src/services/youtube.service.js
@@ -5,7 +5,7 @@ export const youtubeService = {
 async function query(searchStr, maxResults = 10) {
   const apiKey = "AIzaSyDyENi9CUDP6AY90P7xZm3x326KlwI_d2Q"; // Replace with your actual API key
   const response = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchStr}&key=${apiKey}&maxResults=${maxResults}`
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchStr}&key=${apiKey}&maxResults=${maxResults}&type=video`
   );
   const data = await response.json();
   return data.items;
