49b333295ec1d051b161236d4776cec720925931
barmoshe <1barmoshe1@gmail.com>
2024-04-03 12:57:03 +0300

    changed demo data logo

--- src/services/station.service.js
+++ src/services/station.service.js
@@ -1,99 +1,94 @@
 import { storageService } from "./async-storage.service";
 import { utilService } from "./util.service";
 
-const STORAGE_KEY = 'stations'
+import tunmbnail from "../assets/imgs/logo-Blue3D.png";
+
+const STORAGE_KEY = "stationsDB";
 
 export const stationService = {
   query,
   save,
   remove,
   getById,
-  getDefaultFilter
-}
+  getDefaultFilter,
+};
 
-_createStations()
-
+_createStations();
 
 async function query(filterBy) {
-  let stations = await storageService.query(STORAGE_KEY)
+  let stations = await storageService.query(STORAGE_KEY);
   console.log("query:", stations);
   if (filterBy) {
-    stations = filterEmails(stations, filterBy)
+    stations = filterEmails(stations, filterBy);
   }
-  return stations
+  return stations;
 }
 
 function save(stationToSave) {
-  if (stationToSave.id)
-    return storageService.put(STORAGE_KEY, stationToSave)
-  else
-    return storageService.post(STORAGE_KEY, stationToSave)
+  if (stationToSave.id) return storageService.put(STORAGE_KEY, stationToSave);
+  else return storageService.post(STORAGE_KEY, stationToSave);
 }
 
-
 function remove(id) {
-  return storageService.remove(STORAGE_KEY, id)
+  return storageService.remove(STORAGE_KEY, id);
 }
 
 function getById(id) {
-  return storageService.get(STORAGE_KEY, id)
+  return storageService.get(STORAGE_KEY, id);
 }
 
-
 function getDefaultFilter() {
-  return {}
+  return {};
 }
 
 function _createStations() {
-  var stations = utilService.loadFromStorage(STORAGE_KEY)
-  const demoDataCount = 10
+  var stations = utilService.loadFromStorage(STORAGE_KEY);
+  const demoDataCount = 10;
 
   if (!stations || !stations.length) {
-    stations = []
+    stations = [];
     for (var i = 0; i < demoDataCount; i++) {
-      stations.push(_createStation())
+      stations.push(_createStation());
       console.log(`station ${i}`, stations[i]);
     }
-
   }
 
-  utilService.saveToStorage(STORAGE_KEY, stations)
+  utilService.saveToStorage(STORAGE_KEY, stations);
 
-  return stations
+  return stations;
 }
 
 function _createStation() {
-  return (
-    {
-      _id: utilService.makeId(),
-      name: "Funky Monks",
-      type: "playlist",
-      tags: ["Funk", "Happy"],
-      createdBy: {
-        _id: "u101",
-        fullname: "Puki Ben David",
-        imgUrl: "http://some-photo/",
+  return {
+    _id: utilService.makeId(),
+    name: "Funky Monks",
+    type: "playlist",
+    tags: ["Funk", "Happy"],
+    createdBy: {
+      _id: "u101",
+      fullname: "Puki Ben David",
+      imgUrl: "http://some-photo/",
+    },
+    likedByUsers: ["{minimal-user}", "{minimal-user}"],
+    songs: [
+      {
+        id: "s1001",
+        title: "The Meters - Cissy Strut",
+        url: "youtube/song.mp4",
+        imgUrl: tunmbnail,
+        addedBy: "{minimal-user}",
+        addedAt: 162521765262,
+        tags: ["Funk", "Happy", "hip-hop"],
       },
-      likedByUsers: ["{minimal-user}", "{minimal-user}"],
-      songs: [
-        {
-          id: "s1001",
-          title: "The Meters - Cissy Strut",
-          url: "youtube/song.mp4",
-          imgUrl: "https://i.ytimg.com/vi/4_iC0MyIykM/mqdefault.jpg",
-          addedBy: "{minimal-user}",
-          addedAt: 162521765262,
-          tags: ["Funk", "Happy", "hip-hop"]
-        },
-        {
-          id: "mUkfiLjooxs",
-          title: "The JB's - Pass The Peas",
-          url: "youtube/song.mp4",
-          imgUrl: "https://i.ytimg.com/vi/mUkfiLjooxs/mqdefault.jpg",
-          addedBy: {},
-          addedAt: 162521765262,
-          tags: ["Funk", "Happy", "rock"],
-        },
-      ],
-    })
+      {
+        id: "mUkfiLjooxs",
+        title: "The JB's - Pass The Peas",
+        url: "youtube/song.mp4",
+        imgUrl: tunmbnail,
+        addedBy: {},
+        addedAt: 162521765262,
+        tags: ["Funk", "Happy", "rock"],
+      },
+    ],
+  };
 }
