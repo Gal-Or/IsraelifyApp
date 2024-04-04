1cb8a1e8a28a83d481141ec00b5ee9cbfda04bb7
barmoshe <1barmoshe1@gmail.com>
2024-04-04 20:58:32 +0300

    change played song from search

--- src/cmps/Player.jsx
+++ src/cmps/Player.jsx
@@ -1,10 +1,30 @@
+import { useSelector } from "react-redux";
+
 import { YouTubePlayer } from "./YouTubePlayer";
+import { useEffect, useState } from "react";
 
 export function Player() {
+  const youtubePlayer = useSelector(
+    (state) => state.playerModule.youtubePlayer
+  );
+
+  function onPlay() {
+    youtubePlayer.playVideo();
+  }
+  function onPause() {
+    youtubePlayer.pauseVideo();
+  }
+  function onStop() {
+    youtubePlayer.stopVideo();
+  }
+
   return (
     <div>
       <h1>Player</h1>
-      <YouTubePlayer videoId="dQw4w9WgXcQ" />
+      <YouTubePlayer />
+      <button onClick={onPlay}>Play</button>
+      <button onClick={onPause}>Pause</button>
+      <button onClick={onStop}>Stop</button>
     </div>
   );
 }
--- src/cmps/SongResults.jsx
+++ src/cmps/SongResults.jsx
@@ -1,9 +1,17 @@
 import { addSongToStation } from "../store/station.actions";
+import { setCurrentSong } from "../store/player.actions";
+import { useEffect } from "react";
 
 export function SongResults({ songResults }) {
+  useEffect(() => {
+    console.log("SongResults ---->", songResults);
+  }, []);
   //
   async function onAddToPlaylist(song) {
     await addSongToStation(song);
+  }
+  function onPlaySong(song) {
+    setCurrentSong(song);
   }
 
   return (
@@ -13,11 +21,15 @@ export function SongResults({ songResults }) {
           <img src={song.img} alt={song.name} style={{ width: "100px" }} />
           <h3>{song.name}</h3>
           <p>{song.artist}</p>
+
           <button
             className="add-to-playlist"
             onClick={() => onAddToPlaylist(song)}
           >
             Add to playlist
+          </button>
+          <button className="play-song" onClick={() => onPlaySong(song)}>
+            Play song
           </button>
         </article>
       ))}
