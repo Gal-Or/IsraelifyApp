a906304c768d07b34a595ed86918fef951dfb349
barmoshe <1barmoshe1@gmail.com>
2024-04-04 20:57:19 +0300

    change api key

--- src/services/youtube.service.js
+++ src/services/youtube.service.js
@@ -1,17 +1,17 @@
 export const youtubeService = {
   query,
-  cleanUpResults
+  cleanUpResults,
 };
+const API_KEY = "AIzaSyCIEC-IUYCVUJMIO8J-2Yn7w_SF-jUeKRw";
 
 async function query(searchStr, maxResults = 10) {
-  const apiKey = "AIzaSyDyENi9CUDP6AY90P7xZm3x326KlwI_d2Q"; // Replace with your actual API key
   const response = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchStr}&key=${apiKey}&maxResults=${maxResults}&type=video`
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchStr}&key=${API_KEY}&maxResults=${maxResults}&type=video`
   );
+
   const data = await response.json();
   return data.items;
 }
-
 
 function cleanUpResults(results) {
   console.log("before clean", results);
