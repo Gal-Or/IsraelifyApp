e631cfd725ba351af6bd68d9200355eceb95d262
barmoshe <1barmoshe1@gmail.com>
2024-04-09 12:14:01 +0300

    Update Library.jsx

--- src/cmps/Library.jsx
+++ src/cmps/Library.jsx
@@ -2,28 +2,25 @@ import { useSelector } from "react-redux";
 
 import { stationService } from "../services/station.service";
 
-import { addStation } from "../store/station.actions"
+import { addStation } from "../store/station.actions";
 
-import { StationList } from "./StationList"
+import { StationList } from "./StationList";
 import { useNavigate } from "react-router";
 
 export function Library() {
-
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
 
   async function onCreateStation() {
-    var newStation = stationService.createDefaultStation()
-    const id = await addStation(newStation)
-    navigate(`station/${id}`)
+    var newStation = stationService.createDefaultStation();
+    const id = await addStation(newStation);
+    navigate(`station/${id}`);
   }
 
   return (
     <section className="library-container">
       <section className="library-header">
         Library Header
-        <button onClick={() => onCreateStation()} >Create Playlist</button>
-
+        <button onClick={() => onCreateStation()}>Create Playlist</button>
       </section>
 
       <StationList />
