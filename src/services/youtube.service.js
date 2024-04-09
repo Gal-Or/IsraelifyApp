60fd2d8f17d3279a5c072e224919e3ba634e9963
barmoshe <1barmoshe1@gmail.com>
2024-04-09 12:04:44 +0300

    Fix format duration bug

--- src/services/youtube.service.js
+++ src/services/youtube.service.js
@@ -57,6 +57,7 @@ async function getDurations(results) {
 function formatDuration(duration) {
   if (!duration) return 0;
   const match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
+  if (!match) return 0;
   const hours = parseInt(match[1]) || 0;
   const minutes = parseInt(match[2]) || 0;
   const seconds = parseInt(match[3]) || 0;
