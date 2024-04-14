67af6c284077585f9f43ef6b9c78c2e04e6899e6
galor <galor.dvlp@gmail.com>
2024-04-14 17:47:34 +0300

    Add buttons (icons) to App Header and style

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,5 +1,9 @@
 import { useNavigate, useParams } from "react-router";
 import { useState } from "react";
+import { ReactSVG } from "react-svg";
+
+import rightArrow from "../assets/icons/right_arrow.svg"
+import leftArrow from "../assets/icons/left_arrow.svg"
 
 export function AppHeader() {
   const navigate = useNavigate();
@@ -21,12 +25,22 @@ export function AppHeader() {
 
   return (
     <header className="app-header">
+
+      <button className="left-arrow-btn">
+        <ReactSVG src={leftArrow} />
+      </button>
+      <button className="right-arrow-btn">
+        <ReactSVG src={rightArrow} />
+      </button>
+
       <input
         type="text"
         placeholder="Search"
         onChange={onInputChange}
         value={currentQuery}
       />
+
+
     </header>
   );
 }
