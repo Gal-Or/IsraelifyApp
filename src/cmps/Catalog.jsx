935ccb6ad6ed48c61a9d10a1d1a118c137059b08
galor <galor.dvlp@gmail.com>
2024-04-17 15:11:18 +0300

    Style home page catalog - lists of playlists

--- src/cmps/Catalog.jsx
+++ src/cmps/Catalog.jsx
@@ -2,11 +2,21 @@ import { StationList } from "./StationList";
 
 export function Catalog() {
   return (
-    <div>
-      {/* <StationList />
-      <StationList />
-      <StationList />
-      <StationList /> */}
+    <div className="catalog">
+      <h3>Made for you</h3>
+      <div className="list-container">
+        <StationList width={200} />
+      </div>
+
+      <h3>Yout top mixes</h3>
+      <div className="list-container">
+        <StationList />
+      </div>
+
+      <h3>Recently played</h3>
+      <div className="list-container">
+        <StationList />
+      </div>
     </div>
   );
 }
