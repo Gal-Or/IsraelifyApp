34184af0146a8af153d6939933a77d9db8d13c2f
barmoshe <1barmoshe1@gmail.com>
2024-04-18 17:53:07 +0300

    update player style

--- src/cmps/MinimalSong.jsx
+++ src/cmps/MinimalSong.jsx
@@ -1,10 +1,35 @@
 import { useSelector } from "react-redux";
 import { ReactSVG } from "react-svg";
 
+import { addSongToStation } from "../store/station.actions";
+
 import addToPlaylistIcon from "../assets/icons/plusWithBorderIcon.svg";
+import tickIcon from "../assets/icons/tickIcon.svg";
+import { useEffect, useState } from "react";
 
 export function MinimalSong() {
   const song = useSelector((state) => state.playerModule.currentSong);
+  const stations = useSelector((state) => state.stationModule.stations);
+  const [likedSongs, setLikedSongs] = useState(null);
+
+  useEffect(() => {
+    if (!stations) return;
+    if (stations.length === 0) return;
+    //find station with id "liked-songs"
+    const likedSongsStation = stations.find(
+      (station) => station._id === "liked-songs"
+    );
+    if (!likedSongsStation) return;
+    setLikedSongs(likedSongsStation.songs);
+  }, [stations]);
+
+  function onAddToPlaylist() {
+    if (likedSongs.find((likedSong) => likedSong.id === song.id)) {
+      return;
+    }
+    addSongToStation(song, "liked-songs");
+  }
+
   return (
     <section className="minimal-song">
       <div className="song-img">
@@ -14,8 +39,13 @@ export function MinimalSong() {
         <p>{song.name}</p>
         <small>{song.artist}</small>
       </div>
-      <button className="add-to-playlist">
-        <ReactSVG src={addToPlaylistIcon} />
+      <button className="add-to-playlist" onClick={onAddToPlaylist}>
+        {likedSongs &&
+        likedSongs.find((likedSong) => likedSong.id === song.id) ? (
+          <ReactSVG src={tickIcon} className="liked-icon" />
+        ) : (
+          <ReactSVG src={addToPlaylistIcon} />
+        )}
       </button>
     </section>
   );
