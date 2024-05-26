80bc571d0811331c54b25aeb0a72a46b9a962314
galor <galor.dvlp@gmail.com>
2024-05-26 17:48:55 +0300

    Fix classname for 'show all' link

--- src/cmps/Catalog.jsx
+++ src/cmps/Catalog.jsx
@@ -5,19 +5,19 @@ export function Catalog() {
     <div className="catalog">
       <div className="title-container">
         <h3>Made for you</h3>
-        <a className="show">Show all</a>
+        <a className="show-all-link">Show all</a>
       </div>
       <StationList width={200} />
 
       <div className="title-container">
         <h3>Yout top mixes</h3>
-        <a className="show">Show all</a>
+        <a className="show-all-link">Show all</a>
       </div>
       <StationList />
 
       <div className="title-container">
         <h3>Recently played</h3>
-        <a className="show">Show all</a>
+        <a className="show-all-link">Show all</a>
       </div>
       <StationList />
 
