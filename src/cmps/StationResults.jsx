e661630f2ae120d3d428fe48c2fdaf0b8b4bb2cc
barmoshe <1barmoshe1@gmail.com>
2024-05-26 14:51:23 +0300

    fix station result list

--- src/cmps/StationResults.jsx
+++ src/cmps/StationResults.jsx
@@ -1,16 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { ReactSVG } from "react-svg";
 import tempStationImg from "../assets/imgs/logo-Blue3D.png";
-
 import playIcon from "../assets/icons/playIcon.svg";
 
 export function StationResults({ stationResults }) {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef.current && containerRef.current.firstElementChild) {
+      const firstChild = containerRef.current.firstElementChild;
+      const firstChildHeight = firstChild.offsetHeight;
+      containerRef.current.style.maxHeight = `${firstChildHeight}px`;
+    }
+  }, [stationResults]);
+
   if (!stationResults) return <div>Loading...</div>;
+
   return (
     <section className="station-results">
       <h1>Stations</h1>
-      <div className="station-results-container">
+      <div className="station-results-container" ref={containerRef}>
         {stationResults &&
           stationResults.map((station, index) => (
             <article key={station._id} className="station-card">
