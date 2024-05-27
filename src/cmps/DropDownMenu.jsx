b446325421bb813e889ebe5a7e07003ee0ced0ab
barmoshe <1barmoshe1@gmail.com>
2024-05-27 13:18:31 +0300

    addd more menu to station action and style

--- src/cmps/DropDownMenu.jsx
+++ src/cmps/DropDownMenu.jsx
@@ -4,13 +4,14 @@ import { ReactSVG } from "react-svg";
 
 import tickIcon from "../assets/icons/tick.svg";
 export function Dropdown({
-  options,
-  onSelect,
-  headline,
-  toggle,
-  className,
-  toggleTick = false,
-  closeOnSelect = true,
+  options, // An array of objects representing the dropdown options. Each object should have 'label' and 'value' properties, and optionally an 'icon' property.
+  onSelect, // A callback function that is triggered when an option is selected. Receives the selected option as an argument.
+  headline, // An optional string that, if provided, will be displayed as a headline at the top of the dropdown menu.
+  toggle, // An optional element or string to customize the dropdown toggle button. If not provided, the selected option's label or value will be used.
+  className, // An optional string for custom CSS classes to style the dropdown component.
+  toggleTick = false, // A boolean that determines whether to show a tick icon next to the selected option in the dropdown menu.
+  closeOnSelect = true, // A boolean that determines whether the dropdown menu should close after an option is selected.
+  showSelected = true, // A boolean that determines whether the selected option should be displayed in the dropdown toggle button.
 }) {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState(options[0]);
@@ -47,7 +48,7 @@ export function Dropdown({
           : selectedOption
           ? selectedOption.label
           : selectedOption.value}
-        {selectedOption.icon && (
+        {selectedOption.icon && !toggle && (
           <ReactSVG src={selectedOption.icon} className="dropdown-icon" />
         )}
       </button>
@@ -60,7 +61,9 @@ export function Dropdown({
                 key={option.value}
                 onClick={() => handleOptionClick(option)}
                 className={
-                  option.value === selectedOption.value ? "selected" : ""
+                  option.value === selectedOption.value && showSelected
+                    ? "selected"
+                    : ""
                 }
               >
                 {option.icon && (
--- src/cmps/StationActions.jsx
+++ src/cmps/StationActions.jsx
@@ -7,14 +7,37 @@ import pauseIcon from "../assets/icons/pauseIcon.svg";
 import DotsIcon from "../assets/icons/Ellipses.svg";
 import listIcon from "../assets/icons/list.svg";
 import compactIcon from "../assets/icons/compact.svg";
+import deleteIcon from "../assets/icons/delete.svg";
+import editIcon from "../assets/icons/pencil.svg";
+import addIcon from "../assets/icons/AddToQueue.svg";
+
 export function StationActions({ station, setIsCompact, isCompact }) {
-  const options = [
+  const viewOptions = [
     { label: "List", value: false, icon: listIcon },
     { label: "Compact", value: true, icon: compactIcon },
   ];
+  const moreOptions = [
+    { label: "delete", value: "delete", icon: deleteIcon },
+    { label: "edit", value: "edit", icon: editIcon },
+    { label: "add to queue", value: "add to queue", icon: addIcon },
+  ];
 
-  const handleSelect = (option) => {
+  const handleViewSelect = (option) => {
     setIsCompact(option.value);
+  };
+
+  const handleMoreSelect = (option) => {
+    switch (option.value) {
+      case "delete":
+        console.log("delete");
+        break;
+      case "edit":
+        console.log("edit");
+        break;
+      case "add to queue":
+        console.log("add to queue");
+        break;
+    }
   };
 
   return (
@@ -22,15 +45,23 @@ export function StationActions({ station, setIsCompact, isCompact }) {
       <button className="play-btn">
         <ReactSVG src={playIcon} />
       </button>
-      <button className="more-btn">
-        {" "}
-        <ReactSVG src={DotsIcon} />{" "}
-      </button>
+      <div className="more-options">
+        <Dropdown
+          options={moreOptions}
+          onSelect={handleMoreSelect}
+          toggle={<ReactSVG src={DotsIcon} />}
+          toggleTick={false}
+          closeOnSelect={true}
+          showSelected={false}
+          key={station._id + "more"}
+        />
+      </div>
       <div className="change-view">
         <Dropdown
-          options={options}
-          onSelect={handleSelect}
+          options={viewOptions}
+          onSelect={handleViewSelect}
           headline="View as"
+          key={station._id + "view"}
         />
       </div>
     </div>
