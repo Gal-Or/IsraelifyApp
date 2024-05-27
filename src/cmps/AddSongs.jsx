19d43f3ba77adbe4cbd8c4e7b23c5668c538ddc8
barmoshe <1barmoshe1@gmail.com>
2024-05-27 13:18:13 +0300

    -style- add songs cmp

--- src/cmps/AddSongs.jsx
+++ src/cmps/AddSongs.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { youtubeService } from "../services/youtube.service.js";
+import { ReactSVG } from "react-svg";
+import search from "../assets/icons/search.svg";
 
 import { SongResults } from "./SongResults.jsx";
 
@@ -19,14 +21,27 @@ export function AddSongs({ onAddSongToStation }) {
 
   return (
     <>
-      <h1>Add Songs</h1>
-      <input type="text" placeholder="Search" onChange={onInputChange} />
-      {songResults && (
-        <SongResults
-          songResults={songResults}
-          onAddSongToStation={onAddSongToStation}
-        />
-      )}
+      <div className="add-songs-header-line">
+        <hr />
+      </div>
+      <div className="add-songs">
+        <h1>Let's find something for your playlist</h1>
+
+        <div className="search-container">
+          <ReactSVG src={search} />
+          <input
+            type="text"
+            placeholder="Search for songs"
+            onChange={onInputChange}
+          />
+        </div>
+        {songResults && (
+          <SongResults
+            songResults={songResults}
+            onAddSongToStation={onAddSongToStation}
+          />
+        )}
+      </div>
     </>
   );
 }
