09aec4d550ccf34882ed760379209eab62a0a652
galor <galor.dvlp@gmail.com>
2024-05-28 19:28:47 +0300

    Add style To miniPreview in homepage

--- src/cmps/MiniPreview.jsx
+++ src/cmps/MiniPreview.jsx
@@ -1,4 +1,6 @@
+import { ReactSVG } from "react-svg";
 import tempImg from "../assets/imgs/logo-Blue3D.png";
+import playIcon from "../assets/icons/playIcon.svg";
 
 
 export function MiniPreview() {
@@ -7,5 +9,11 @@ export function MiniPreview() {
         <article className="mini-preview">
             <img src={tempImg} />
             <span>name</span>
+            <button
+                className="play-btn"
+                onClick={() => console.log(station)}
+            >
+                <ReactSVG src={playIcon} />
+            </button>
         </article>)
 }
