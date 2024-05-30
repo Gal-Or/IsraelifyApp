66445f0b66aaf7424d5bb5ff567b8b4b9a1a7502
barmoshe <1barmoshe1@gmail.com>
2024-05-30 13:24:44 +0300

    Dont show footer in sign pages

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,6 +1,12 @@
 import { FullPlayer } from "./FullPlayer";
+import { useLocation } from "react-router-dom";
 
 export function AppFooter() {
+  //show footer only if it's not the in the sign pages , use react hook
+  const isSignPage = useLocation().pathname.includes("sign");
+
+  if (isSignPage) return null;
+
   return (
     <footer className="app-footer">
       <FullPlayer />
