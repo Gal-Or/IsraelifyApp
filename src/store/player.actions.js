0ba310295f891551335ea419beb7a8b14526381d
barmoshe <1barmoshe1@gmail.com>
2024-06-02 13:33:31 +0300

    fix issue

--- src/store/player.actions.js
+++ src/store/player.actions.js
@@ -1,5 +1,5 @@
 import { store } from "../store/store";
-
+import { youtubeService } from "../services/youtube.service";
 export const ADD_TO_QUEUE = "ADD_TO_QUEUE";
 export const REMOVE_FROM_QUEUE = "REMOVE_FROM_QUEUE";
 export const SET_QUEUE = "SET_QUEUE";
