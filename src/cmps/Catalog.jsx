aac21b38fd2fcac26c18c10c49084d0ef52c3b2a
galor <galor.dvlp@gmail.com>
2024-06-03 18:38:04 +0300

    Initial functionallity for 'showAll' btn

--- src/cmps/Catalog.jsx
+++ src/cmps/Catalog.jsx
@@ -1,25 +1,46 @@
 import { StationList } from "./StationList";
+import { useState } from "react";
 
-export function Catalog() {
+export function Catalog({ showAll, onShowAll }) {
+
+  function handleShowAll(value) {
+    if (value === showAll)
+      onShowAll(null)
+    else
+      onShowAll(value)
+  }
+
   return (
+
     <div className="catalog">
-      <div className="title-container">
-        <h3>Made for you</h3>
-        <a className="show-all-link">Show all</a>
-      </div>
-      <StationList width={200} randomize={true} />
 
-      <div className="title-container">
-        <h3>Yout top mixes</h3>
-        <a className="show-all-link">Show all</a>
-      </div>
-      <StationList randomize={true} />
+      {(showAll === 'list1' || showAll === null) &&
+        <>
+          <div className="title-container">
+            <h3>Made for you</h3>
+            <a className="show-all-link" onClick={() => handleShowAll('list1')}>{showAll ? "hide" : "Show All"}</a>
+          </div>
+          <StationList width={200} randomize={true} />
+        </>}
 
-      <div className="title-container">
-        <h3>Recently played</h3>
-        <a className="show-all-link">Show all</a>
-      </div>
-      <StationList randomize={true} />
+      {(showAll === 'list2' || showAll === null) &&
+        <>
+          <div className="title-container">
+            <h3>Yout top mixes</h3>
+            <a className="show-all-link" onClick={() => handleShowAll('list2')}>{showAll ? "hide" : "Show All"}</a>
+          </div>
+          <StationList randomize={true} />
+        </>}
+
+      {(showAll === 'list3' || showAll === null) &&
+        <>
+          <div className="title-container">
+            <h3>Recently played</h3>
+            <a className="show-all-link" onClick={() => handleShowAll('list3')}>{showAll ? "hide" : "Show All"}</a>
+          </div>
+          <StationList randomize={true} />
+        </>}
+
     </div>
   );
 }
