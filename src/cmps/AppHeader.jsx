3910807cc67dd57a75760d8bb89956c1cbc9b935
galor <galor.dvlp@gmail.com>
2024-06-03 16:10:17 +0300

    Add profile btn to app header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,15 +1,20 @@
 import { useNavigate, useParams } from "react-router";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useContext } from "react";
 import { ReactSVG } from "react-svg";
 import { debounce } from "lodash";
+import { UserContext } from '../RootCmp.jsx'
+import { userService } from "../services/user-local.service.js";
 
 import rightArrow from "../assets/icons/right_arrow.svg";
 import leftArrow from "../assets/icons/left_arrow.svg";
 import search from "../assets/icons/search.svg";
+import { ContextMenu } from "./ContextMenu.jsx";
 
 export function AppHeader() {
   const navigate = useNavigate();
   const params = useParams();
+  const [contextMenu, setContextMenu] = useState(null);
+  const [loggedinUser, setLoggedinUser] = useContext(UserContext)
 
   const [currentQuery, setCurrentQuery] = useState(
     params.query ? formatQuery(params.query) : ""
@@ -35,6 +40,27 @@ export function AppHeader() {
     debouncedNavigate(value);
   }
 
+  const handleContextMenu = (event) => {
+    event.preventDefault();
+    setContextMenu({
+      position: { x: event.clientX - 450, y: event.clientY + 25 },
+      options: [
+        {
+          label: "Log out",
+          value: "log out",
+          onClick: async () => {
+            await userService.logout()
+            navigate('/signin')
+          },
+        }
+      ],
+    });
+  };
+
+  const handleCloseContextMenu = () => {
+    setContextMenu(null);
+  };
+
   return (
     <header className="app-header">
       <button className="left-arrow-btn" onClick={() => navigate(-1)}>
@@ -53,6 +79,18 @@ export function AppHeader() {
           value={formatQuery(currentQuery)}
         />
       </div>
+
+
+      <button className="profile-btn" onClick={handleContextMenu}><img src={loggedinUser.imgUrl} alt="profile" /></button>
+
+      {contextMenu && (
+        <ContextMenu
+          position={contextMenu.position}
+          options={contextMenu.options}
+          onClose={handleCloseContextMenu}
+        />
+      )}
+
     </header>
   );
 }
