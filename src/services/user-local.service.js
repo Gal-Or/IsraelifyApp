46d5911061afce124914f12f4ad0fab3bdfa9ec6
barmoshe <1barmoshe1@gmail.com>
2024-06-07 11:52:10 +0300

    Add usescrool hook to update the header when scrolled down  and update sign in page to not apear at first(and guest handling)

--- src/services/user-local.service.js
+++ src/services/user-local.service.js
@@ -1,86 +1,106 @@
-import { storageService } from './async-storage.service'
+import { storageService } from "./async-storage.service";
 
-const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
-const STORAGE_KEY_USER_DB = 'user'
+const STORAGE_KEY_LOGGEDIN_USER = "loggedinUser";
+const STORAGE_KEY_USER_DB = "user";
 
 export const userService = {
-	login,
-	logout,
-	signup,
-	getLoggedinUser,
-	saveLocalUser,
-	getUsers,
-	getById,
-	remove,
-	update,
-	getEmptyUser
-}
+  login,
+  logout,
+  signup,
+  getLoggedinUser,
+  saveLocalUser,
+  getUsers,
+  getById,
+  remove,
+  update,
+  getEmptyUser,
+};
 
-window.userService = userService
+window.userService = userService;
 
 function getUsers() {
-	return storageService.query(STORAGE_KEY_USER_DB)
+  return storageService.query(STORAGE_KEY_USER_DB);
 }
 
 async function getById(userId) {
-	const user = await storageService.get(STORAGE_KEY_USER_DB, userId)
-	return user
+  const user = await storageService.get(STORAGE_KEY_USER_DB, userId);
+  return user;
 }
 
 function remove(userId) {
-	return storageService.remove(STORAGE_KEY_USER_DB, userId)
+  return storageService.remove(STORAGE_KEY_USER_DB, userId);
 }
 
 async function update(userToUpdate) {
-	const user = await getById(userToUpdate.id)
+  const user = await getById(userToUpdate.id);
 
-	const updatedUser = await storageService.put(STORAGE_KEY_USER_DB, { ...user, ...userToUpdate })
-	if (getLoggedinUser().id === updatedUser.id) saveLocalUser(updatedUser)
-	return updatedUser
+  const updatedUser = await storageService.put(STORAGE_KEY_USER_DB, {
+    ...user,
+    ...userToUpdate,
+  });
+  if (getLoggedinUser().id === updatedUser.id) saveLocalUser(updatedUser);
+  return updatedUser;
 }
 
 function getEmptyUser() {
-	return {
-		username: '',
-		fullname: '',
-		password: '',
-		imgUrl: 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
-	}
+  return {
+    username: "",
+    fullname: "",
+    password: "",
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png",
+  };
 }
 
 // Auth functions
 
 async function signup(userCred) {
-	// Adding "Server data"
-	if (!userCred.imgUrl) userCred.imgUrl = 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'
-	// Creating a new user
-	const user = await storageService.post('user', userCred)
-	// Performing login -> Saving in the session storage
-	return saveLocalUser(user)
+  // Adding "Server data"
+  if (!userCred.imgUrl)
+    userCred.imgUrl =
+      "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png";
+  // Creating a new user
+  const user = await storageService.post("user", userCred);
+  // Performing login -> Saving in the session storage
+  return saveLocalUser(user);
 }
 
 async function login(userCred) {
-	const users = await storageService.query(STORAGE_KEY_USER_DB)
-	const user = users.find(user => user.username === userCred.username)
-	if (user) {
-		return saveLocalUser(user)
-	}
+  const users = await storageService.query(STORAGE_KEY_USER_DB);
+  const user = users.find((user) => user.username === userCred.username);
+  if (user) {
+    return saveLocalUser(user);
+  }
 }
 
 async function logout() {
-	sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
+  sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER);
+  //login as guest
+  return saveLocalUser({
+    id: "u101",
+    fullname: "Guest",
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png",
+  });
 }
 
 function saveLocalUser(user) {
-	user = { id: user.id, fullname: user.fullname, imgUrl: user.imgUrl }
-	sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
-	return user
+  user = { id: user.id, fullname: user.fullname, imgUrl: user.imgUrl };
+  sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user));
+  return user;
 }
 
 function getLoggedinUser() {
-	return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER))
+  if (sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER)) {
+    return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER));
+  }
+  return {
+    id: "u101",
+    fullname: "Guest",
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png",
+  };
 }
-
 
 // ;(async ()=>{
 //     await userService.signup({fullname: 'Puki Norma', username: 'puki', password:'123', isAdmin: false})
