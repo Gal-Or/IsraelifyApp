e1b888adbf2a6122dfc3df09b837ef2bd3bfb5d6
barmoshe <1barmoshe1@gmail.com>
2024-06-11 12:14:11 +0300

    catalog looks and capitalize recommendation’s first letter

--- src/cmps/MiniPreview.jsx
+++ src/cmps/MiniPreview.jsx
@@ -1,11 +1,12 @@
 import { ReactSVG } from "react-svg";
 import playIcon from "../assets/icons/PlayIcon.svg";
+import { utilService } from "../services/util.service";
 
 export function MiniPreview({ genre }) {
   return (
     <article className="mini-preview">
       <img src={genre.photo} alt={genre.name} />
-      <span>{genre.name}</span>
+      <span>{utilService.capitalize(genre.name)}</span>
       <button className="play-btn" onClick={() => console.log("play")}>
         <ReactSVG src={playIcon} />
       </button>
